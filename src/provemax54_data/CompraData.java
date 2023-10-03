1e39ceb749d2fc92d617e554fd1a40069e8cdcd7
Pablo Sisto <pablosisto0@gmail.com>
2023-10-03 19:47:26 -0300

    cambio en Conexion

--- src/provemax54_data/CompraData.java
+++ src/provemax54_data/CompraData.java
@@ -5,10 +5,35 @@
  */
 package provemax54_data;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import provemax54_entidades.CompraEntidades;
+
 /**
  *
  * @author Ideapad 5
  */
 public class CompraData {
+    
+    Conexion connection = null;
+    
+    public CompraData() {
+        
+       connection =  Conexion.getConexion();
+    }
+    
+    private void mensaje(String mensaje) {
+        JOptionPane.showMessageDialog(null, mensaje);
+    }
+    
+    
+    public void guardarCompra(CompraEntidades compra) {
+        
+        String sql = " ";
+        
+    }
     
 }
--- src/provemax54_data/Conexion.java
+++ src/provemax54_data/Conexion.java
@@ -8,33 +8,47 @@ package provemax54_data;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Ideapad 5
  */
 public class Conexion {
-    private static  String url = "jdbc:mariadb://localhost:3306/provemax54";   
-        private static  String usuario = "root";
-        private static  String password = "";
-        private static Connection connection;
-    
-        private Conexion() throws ClassNotFoundException {
+
+        private static String url = "jdbc:mariadb://localhost:3307/provemax54";
+    private static String usuario = "root";
+    private static String password = "";
+    private static Connection connection;
+
+    private Conexion() {
+        try {
             Class.forName("org.mariadb.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        private Conexion(String url, String usuario, String password) throws ClassNotFoundException {
-            this.url = url;
-            this.usuario = usuario;
-            this.password = password;
-            Class.forName("org.mariadb.jdbc.Driver");
-        }
-        
-          public static Connection getConexion() throws SQLException, ClassNotFoundException {
-            if (connection == null) {
+    }
+
+    private Conexion(String url, String usuario, String password) throws ClassNotFoundException {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+        Class.forName("org.mariadb.jdbc.Driver");
+    }
+
+    public static Connection getConexion() {
+        if (connection == null) {
+            try {
                 Class.forName("org.mariadb.jdbc.Driver");
+
                 connection = DriverManager.getConnection(url, usuario, password);
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
             }
-            return connection;
         }
+        return connection;
+    }
 }
