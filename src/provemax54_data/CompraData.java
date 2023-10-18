01767e4cbb88329ae20449949b74ef655cd22b3c
Pablo Sisto <pablosisto0@gmail.com>
2023-10-18 19:50:33 -0300

    cambios en compraData

--- src/provemax54_data/CompraData.java
+++ src/provemax54_data/CompraData.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 import provemax54_entidades.CompraEntidades;
+import provemax54_entidades.ProductoEntidades;
 
 /**
  *
@@ -86,30 +87,102 @@ public class CompraData {
         }
         return compra;
     }
-    
+
     public void eliminarCompra(int id) {
         //Paso el id del alumno que quiero eliminar en el metodo 
-        
+
         String sql = "UPDATE compra SET estado = 0 WHERE idCompra = ? ";
-        
-        try(PreparedStatement ps = connection.prepareStatement(sql)) {
+
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setInt(1, id);
-           int exito = ps.executeUpdate();
-           
-            if (exito ==1) {
+            int exito = ps.executeUpdate();
+
+            if (exito == 1) {
                 mensaje("Se elimino la compra");
-            }else {
+            } else {
                 mensaje("La compra no se pudo eliminar");
             }
-            
+
         } catch (SQLException e) {
             mensaje("Error al acceder a la tabla compra " + e.getMessage());
         }
     }
+
+    // Todos los productos de una fecha / todos los productos de una compra en particular
+    public List<ProductoEntidades> compraPorFecha(Date fecha) {
+
+        ArrayList<ProductoEntidades> productos = new ArrayList<>();
+        ProductoEntidades prod = new ProductoEntidades();
+
+        String sql = "SELECT p.* FROM producto AS p JOIN detallecompra AS dc ON p.idProducto = dc.idProducto JOIN compra AS c ON dc.idCompra = c.idCompra WHERE DATE(c.fecha) = ?";
+
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setDate(1, fecha);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                prod.setDescripcion(rs.getString("descripcion"));
+                prod.setNombreProducto(rs.getString("nombreProducto"));
+                prod.setPrecioActual(rs.getDouble("precioActual"));
+                prod.setStock(rs.getInt("stock"));
+
+                productos.add(prod);
+            }
+            ps.close();
+        } catch (SQLException e) {
+            mensaje("Error al acceder a la tabla " + e.getMessage());
+        }
+        return productos;
+    }
+
+    // Productos mas comprados entre dos fechas
+    public List<String[]> productoEntreFechas(Date fecha1, Date fecha2) {
+        List<String[]> productos = new ArrayList<>();
+        String sql = "SELECT p.descripcion, SUM(d.cantidad) AS cantidadT FROM compra c JOIN detallecompra d USING(idCompra) JOIN producto p USING (idProducto) WHERE c.fecha BETWEEN ? AND ? GROUP BY  d.idProducto ORDER BY cantidad DESC;";
+
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setDate(1, fecha1);
+            ps.setDate(2, fecha2);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                productos.add(new String[]{rs.getString("p.descripcion"), "" + rs.getInt("cantidadT")});
+            }
+        } catch (Exception e) {
+        }
+        return productos;
+    }
     
-//    public List<CompraEntidades> compraPorFecha(Date fecha) {
-//        
-//    }
-//    
+   // Productos por debajo del stock 
+    public List<ProductoEntidades> stockMinimo(int stock) {
+
+        ArrayList<ProductoEntidades> productos = new ArrayList<>();
+        ProductoEntidades prod = new ProductoEntidades();
+        
+       // String sql = "SELECT p.* FROM producto p JOIN detallecompra dc ON p.idProducto = dc.idProducto JOIN compra c ON dc.idCompra = c.idCompra WHERE p.estado = 1 AND p.stock < ?;";
+       String sql = "SELECT * FROM producto  WHERE estado = 1 AND stock < ?;"; 
+       
+        try(PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setInt(1, stock);
+            ResultSet rs = ps.executeQuery();
+            
+            while (rs.next()) {                
+                prod.setDescripcion(rs.getString("descripcion"));
+                prod.setNombreProducto(rs.getString("nombreProducto"));
+                prod.setPrecioActual(rs.getDouble("precioActual"));
+                prod.setStock(rs.getInt("stock"));
+                
+                productos.add(prod);
+            }
+            
+        } catch (Exception e) {
+            mensaje("Error al acceder a la tabla " + e.getMessage());
+        }
+        return productos;
+    }
     
+   
+
 }
